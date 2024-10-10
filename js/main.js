8c6e55a317f64fc2bcc8b0f3a07cf8f2c6593444
wanadam00 <wanadam16@gmail.com>
2024-10-10 11:28:05 +0800

    init

--- js/main.js
+++ js/main.js
@@ -1,176 +1,87 @@
-;(function () {
-	
+jQuery(document).ready(function($) {
+
 	'use strict';
 
+        $(window).load(function() { // makes sure the whole site is loaded
+            $(".seq-preloader").fadeOut(); // will first fade out the loading animation
+            $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
+        })
+      
+        
+        $(function() {
+  
+        function showSlide(n) {
+            // n is relative position from current slide
+          
+            // unbind event listener to prevent retriggering
+            $body.unbind("mousewheel");
+          
+            // increment slide number by n and keep within boundaries
+            currSlide = Math.min(Math.max(0, currSlide + n), $slide.length-1);
+            
+            var displacment = window.innerWidth*currSlide;
+            // translate slides div across to appropriate slide
+            $slides.css('transform', 'translateX(-' + displacment + 'px)');
+            // delay before rebinding event to prevent retriggering
+            setTimeout(bind, 700);
+            
+            // change active class on link
+            $('nav a.active').removeClass('active');
+            $($('a')[currSlide]).addClass('active');
+            
+        }
+      
+        function bind() {
+             $body.bind('false', mouseEvent);
+          }
+      
+        function mouseEvent(e, delta) {
+            // On down scroll, show next slide otherwise show prev slide
+            showSlide(delta >= 0 ? -1 : 1);
+            e.preventDefault();
+        }
+        
+        $('nav a, .main-btn a').click(function(e) {
+            // When link clicked, find slide it points to
+            var newslide = parseInt($(this).attr('href')[1]);
+            // find how far it is from current slide
+            var diff = newslide - currSlide - 1;
+            showSlide(diff); // show that slide
+            e.preventDefault();
+        });
+      
+        $(window).resize(function(){
+          // Keep current slide to left of window on resize
+          var displacment = window.innerWidth*currSlide;
+          $slides.css('transform', 'translateX(-'+displacment+'px)');
+        });
+        
+        // cache
+        var $body = $('body');
+        var currSlide = 0;
+        var $slides = $('.slides');
+        var $slide = $('.slide');
+      
+        // give active class to first link
+        $($('nav a')[0]).addClass('active');
+        
+        // add event listener for mousescroll
+        $body.bind('false', mouseEvent);
+    })        
 
 
-	// iPad and iPod detection	
-	var isiPad = function(){
-		return (navigator.platform.indexOf("iPad") != -1);
-	};
-
-	var isiPhone = function(){
-	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
-			(navigator.platform.indexOf("iPod") != -1)
-	    );
-	};
+        $('#form-submit .date').datepicker({
+        });
 
 
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-
-	var burgerMenu = function() {
-
-		$('.js-colorlib-nav-toggle').on('click', function(event) {
-			event.preventDefault();
-			var $this = $(this);
-			if( $('body').hasClass('menu-show') ) {
-				$('body').removeClass('menu-show');
-				$('#colorlib-main-nav > .js-colorlib-nav-toggle').removeClass('show');
-			} else {
-				$('body').addClass('menu-show');
-				setTimeout(function(){
-					$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
-				}, 900);
-			}
-		})
-	};
-
-	// Animations
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated');
-							} else {
-								el.addClass('fadeInUp animated');
-							}
-
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '85%' } );
-	};
+        $(window).on("scroll", function() {
+            if($(window).scrollTop() > 100) {
+                $(".header").addClass("active");
+            } else {
+                //remove the background property so it comes transparent again (defined in your css)
+               $(".header").removeClass("active");
+            }
+        });
 
 
-	var counter = function() {
-		$('.js-counter').countTo({
-			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
-		});
-	};
-
-	var counterWayPoint = function() {
-		if ($('#colorlib-counter').length > 0 ) {
-			$('#colorlib-counter').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-	};
-
-	// Owl Carousel
-	var owlCarouselFeatureSlide = function() {
-		var owl = $('.owl-carousel1');
-		owl.owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:true,
-		   dots: false,
-		   autoHeight: true,
-		   responsive:{
-		      0:{
-		         items:1
-		      },
-		      600:{
-		         items:2
-		      },
-		      1000:{
-		         items:3
-		      }
-		   },
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
-		});
-		var owl2 = $('.owl-carousel');
-		owl2.owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:false,
-		   dots: true,
-		   autoHeight: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
-		});
-		var owl3 = $('.owl-carousel3');
-		owl3.owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:false,
-		   dots: false,
-		   autoHeight: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
-		});	
-	};
-
-	
-
-
-	// Document on load.
-	$(function(){
-		fullHeight();
-		burgerMenu();
-		counterWayPoint();
-		contentWayPoint();
-		owlCarouselFeatureSlide();
-	});
-
-
-}());
+});
